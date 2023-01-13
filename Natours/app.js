9dcfcf7cad9ad5d986c90853bc5a10c448f94644
utkarsh_raj <rajutkarsh2505@gmail.com>
2023-01-13 15:27:42 +0530

    implemented error handling middleware

--- Natours/app.js
+++ Natours/app.js
@@ -17,10 +17,10 @@ app.use(express.static(`${__dirname}/public`)); //it will save public as root fo
 //this is middleware
 //this middleware applies to each and every request
 //.route is also a middleware but it only work for one request
-app.use((req, res, next) => {
-  // console.log('this is middleware');
-  next();
-});
+//app.use((req, res, next) => {
+// console.log('this is middleware');
+// next();
+//});
 
 app.use((req, res, next) => {
   req.requestTIme = new Date().toISOString();
@@ -56,5 +56,32 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter); //This is also a middleware here it means for this route we want to apply tourRouter middleware
 app.use('/api/v1/users', userRouter); //This is also a middleware
+
+// If the code run upto this means request response cycle is not finished yet then it will show html error we have to change it
+// This will run for all the http methods because of all and *
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  // If anything is passed in next function then express will know that it is error
+  next(err);
+});
+
+// four arguments in use means error
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
