c117cc697ab2a15e869f45d7f8357071967b02e8
utkarsh_raj <rajutkarsh2505@gmail.com>
2023-01-14 23:34:39 +0530

    implemented error handling for async functions

--- Natours/app.js
+++ Natours/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -64,24 +67,16 @@ app.all('*', (req, res, next) => {
   //   status: 'fail',
   //   message: `Can't find ${req.originalUrl} on this server`,
   // });
-
-  const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  err.status = 'fail';
-  err.statusCode = 404;
-
+  // const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
   // If anything is passed in next function then express will know that it is error
-  next(err);
+  // next(err);
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
 // four arguments in use means error
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
