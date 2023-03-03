4595542fe903ed18e8a9589f2de1697359a9762a
utkarsh_raj <rajutkarsh2505@gmail.com>
2023-03-03 16:19:47 +0530

    added login and singup authcontroller

--- Natours/controllers/authController.js
+++ Natours/controllers/authController.js
@@ -1,13 +1,47 @@
 const user = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const jwt = require('jsonwebtoken');
+const AppError = require('./../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await user.create(req.body);
+  // const newUser = await user.create(req.body);
+  const newUser = await user.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
   });
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  // check if email password exist
+
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+
+  // check if user exists && password is correct
+
+  const user = user.findOne({ email: email });
+  // if everything is ok send jwt token
+
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+};
--- Natours/models/userModel.js
+++ Natours/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -18,11 +19,31 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Password is required'],
     minlength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
     required: [true, 'confirm password should be same as the password'],
+    validate: {
+      // this will only work on create save
+      validator: function (e) {
+        return e == this.password;
+      },
+      message: 'password must be same',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // this function will only run if the password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // hashing the password with the cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // deleting confirm password field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
