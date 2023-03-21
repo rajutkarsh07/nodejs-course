48b18b5718d8ce3795203c3e888a8ac3e1b590df
utkarsh_raj <rajutkarsh2505@gmail.com>
2023-03-22 03:32:11 +0530

    HTTP headers, data sanitization and parame pollu

--- Natours/app.js
+++ Natours/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,11 +14,19 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-////////////   MIDDLEWARE
+//////////// GLOBAL MIDDLEWARE
+
+/// set security HTTP headers
+
+app.use(helmet());
+
+/// Development Login
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+/// Limit requests from same API
 
 const limiter = rateLimit({
   max: 100,
@@ -24,16 +36,35 @@ const limiter = rateLimit({
 
 app.use('/api', limiter); // this middleware will be applied to only those routes whose url starts with /api
 
-app.use(express.json());
+/// Body parser, reading data from body into req.body
+// app.use(express.json());
+app.use(express.json({ limit: '10kb' })); //limit data upto 190kb
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+// if param has multiple parameter....
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+/// Serving static files
 app.use(express.static(`${__dirname}/public`)); //it will save public as root folder
 
-//this is middleware
-//this middleware applies to each and every request
-//.route is also a middleware but it only work for one request
-//app.use((req, res, next) => {
-// console.log('this is middleware');
-// next();
-//});
+/// Test middleware
 
 app.use((req, res, next) => {
   req.requestTIme = new Date().toISOString();
@@ -42,48 +73,15 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.get('/', (req, res) => {
-//   // res.status(200).send('hello i am server');
-//   res.status(200).json({ message: 'hello i am server', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('you can post to this endpoint...');
-// });
-
-////////////   ROUTE HANDLERS
-
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
-// app.get('/api/v1/tours/:id', getTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
-
 ////////////   ROUTES
-
-// app.route('/api/v1/tours').get(getAllTours).post(createTour);
-// app
-//   .route('/api/v1/tours/:id')
-//   .get(getTour)
-//   .patch(updateTour)
-//   .delete(deleteTour);
 
 app.use('/api/v1/tours', tourRouter); //This is also a middleware here it means for this route we want to apply tourRouter middleware
 app.use('/api/v1/users', userRouter); //This is also a middleware
 
 // If the code run upto this means request response cycle is not finished yet then it will show html error we have to change it
 // This will run for all the http methods because of all and *
-app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server`,
-  // });
-  // const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  // err.status = 'fail';
-  // err.statusCode = 404;
-  // If anything is passed in next function then express will know that it is error
-  // next(err);
 
+app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
